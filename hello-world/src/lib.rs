5a6ed9c99ff500ce001bc985167aa2317e6d9cdd
Innocent Mambo <saneugene847@gmail.com>
2023-06-10 12:37:54 +0000

    testing using gtest

--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -15,38 +15,3 @@ extern "C" fn init() {
         init_message);
 }
 
-static mut GREETING: Option<String> = None;
-
-
-#[no_mangle]
-extern "C" fn init() {
-    let greeting: String = msg::load()
-        .expect("Can't decode an init message");
-    debug!("Program was initialized with message {:?}",
-        greeting);
-    unsafe { GREETING = Some(greeting) };
-}
-
-
-#[no_mangle]
-extern "C" fn handle() {
-    let input_message: InputMessages = msg::load()
-        .expect("Error in loading InputMessages");
-    let greeting = unsafe {
-        GREETING
-            .as_mut()
-            .expect("The contract is not initialized")
-    };
-    match input_message {
-        InputMessages::SendHelloTo(account) => {
-            debug!("Message: SendHelloTo {:?}", account);
-            msg::send(account, greeting, 0)
-                .expect("Error in sending Hello message to account");
-        }
-        InputMessages::SendHelloReply => {
-            debug!("Message: SendHelloReply");
-            msg::reply(greeting, 0)
-                .expect("Error in sending reply");
-        }
-    }
-}
--- hello-world/tests/hello_world_test.rs
+++ hello-world/tests/hello_world_test.rs
@@ -5,5 +5,5 @@ fn hello_test() {
     let sys = System::new();
     sys.init_logger();
     let program = Program::current(&sys);
-    program.send_bytes(2, String::from("INIT MESSAGE"));
+    program.send(2, String::from("INIT MESSAGE"));
 }
